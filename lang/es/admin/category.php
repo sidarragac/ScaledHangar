e48374911b815c0070f7bd8229edf1333742be05
Santiago Idárraga Ceballos <san200526@gmail.com>
2025-03-22 21:29:05 -0500

    Created english and spanish admin-category lang

---
+++ lang/es/admin/category.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'title_index' => 'Categorias',
+    'title_create' => 'Crear Categoria',
+    'title_edit' => 'Editar Categoria',
+    'title_show' => ':Category',
+    'msg_create_success' => 'Categoría creada satisfactoriamente',
+    'msg_edit_success' => 'Categoría editada satisactoriamente',
+    'msg_delete_success' => 'Categoría eliminada satisfactoriamente',
+];
