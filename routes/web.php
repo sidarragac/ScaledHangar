41c9f5b94020e16eb790060c2ee6691ecd94496d
Santiago Idárraga Ceballos <san200526@gmail.com>
2025-03-22 21:52:15 -0500

    Created Admin-Category Routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+$adminCategoryControllerRoute = 'App\Http\Controllers\admin\AdminCategoryController@';
+
+//Admin Category Routes
+
+Route::get('/admin/categories', $adminCategoryControllerRoute . 'index')->name('admin.categories.index');
+Route::get('/admin/categories/create', $adminCategoryControllerRoute . 'create')->name('admin.categories.create');
+Route::post('/admin/categories/save', $adminCategoryControllerRoute . 'save')->name('admin.categories.save');
+Route::get('/admin/categories/edit/{id}', $adminCategoryControllerRoute . 'edit')->name('admin.categories.edit');
+Route::put('/admin/categories/update/{id}', $adminCategoryControllerRoute . 'update')->name('admin.categories.update');
+Route::delete('/admin/categories/delete/{id}', $adminCategoryControllerRoute . 'delete')->name('admin.categories.delete');
