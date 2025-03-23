a8abe07701a29011226359d04d63d4e574aa82be
Santiago Idárraga Ceballos <san200526@gmail.com>
2025-03-23 02:30:15 -0500

    Created Product - Lang

---
+++ lang/en/admin/product.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'title_index' => 'Products',
+    'title_create' => 'Create Product',
+    'title_edit' => 'Edit Product',
+    'title_show' => 'Product :id',
+    'msg_create_success' => 'Product created successfully',
+    'msg_edit_success' => 'Product edited successfully',
+    'msg_delete_success' => 'Product deleted successfully',
+];
