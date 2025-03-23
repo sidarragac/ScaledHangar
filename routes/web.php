1407b2f192274c2ae5c5f82b284bfbdcc2d0f4fd
Santiago Idárraga Ceballos <san200526@gmail.com>
2025-03-23 03:33:24 -0500

    Fixed typos in names

--- routes/web.php
+++ routes/web.php
@@ -6,17 +6,17 @@ $adminCategoryControllerRoute = 'App\Http\Controllers\admin\AdminCategoryControl
 $adminProductControllerRoute = 'App\Http\Controllers\admin\AdminProductController@';
 
 // Admin Category Routes
-Route::get('/admin/categories', $adminCategoryControllerRoute.'index')->name('admin.categories.index');
-Route::get('/admin/categories/create', $adminCategoryControllerRoute.'create')->name('admin.categories.create');
-Route::post('/admin/categories/save', $adminCategoryControllerRoute.'save')->name('admin.categories.save');
-Route::get('/admin/categories/edit/{id}', $adminCategoryControllerRoute.'edit')->name('admin.categories.edit');
-Route::put('/admin/categories/update/{id}', $adminCategoryControllerRoute.'update')->name('admin.categories.update');
-Route::delete('/admin/categories/delete/{id}', $adminCategoryControllerRoute.'delete')->name('admin.categories.delete');
+Route::get('/admin/categories', $adminCategoryControllerRoute.'index')->name('admin.category.index');
+Route::get('/admin/categories/create', $adminCategoryControllerRoute.'create')->name('admin.category.create');
+Route::post('/admin/categories/save', $adminCategoryControllerRoute.'save')->name('admin.category.save');
+Route::get('/admin/categories/edit/{id}', $adminCategoryControllerRoute.'edit')->name('admin.category.edit');
+Route::put('/admin/categories/update/{id}', $adminCategoryControllerRoute.'update')->name('admin.category.update');
+Route::delete('/admin/categories/delete/{id}', $adminCategoryControllerRoute.'delete')->name('admin.category.delete');
 
 // Admin Product Routes
 Route::get('/admin/products', $adminProductControllerRoute.'index')->name('admin.product.index');
-Route::get('/admin/productss/create', $adminProductControllerRoute.'create')->name('admin.product.create');
-Route::post('/admin/productss/save', $adminProductControllerRoute.'save')->name('admin.product.save');
+Route::get('/admin/products/create', $adminProductControllerRoute.'create')->name('admin.product.create');
+Route::post('/admin/products/save', $adminProductControllerRoute.'save')->name('admin.product.save');
 Route::get('/admin/products/edit/{id}', $adminProductControllerRoute.'edit')->name('admin.product.edit');
 Route::put('/admin/products/update/{id}', $adminProductControllerRoute.'update')->name('admin.product.update');
 Route::delete('/admin/products/delete/{id}', $adminProductControllerRoute.'delete')->name('admin.product.delete');
