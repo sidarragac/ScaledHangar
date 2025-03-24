ddd956d9edeaefb1e64126fea6db1709b1b01a6d
Santiago Idárraga Ceballos <101029953+sidarragac@users.noreply.github.com>
2025-03-24 16:54:47 -0500

    Update Product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Facades\Log;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
