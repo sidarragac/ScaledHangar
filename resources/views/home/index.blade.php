75ba42895607e74bdb2a66a1ace3e941768ea1fd
Daviddgp04 <daviddgp04@hotmail.com>
2025-03-24 16:49:37 -0500

    fix: alt on admin/home/index.blade.php

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -6,7 +6,7 @@
     <h1 class="mb-4">{{ __('messages.welcome') }}</h1>
     
     <div class="d-flex justify-content-center">
-        <img src="{{ asset('img/logoText.png') }}" alt="Admin Dashboard" class="img-fluid rounded shadow-lg" style="max-width: 400px;">
+        <img src="{{ asset('img/logoText.png') }}" alt="Company Logo" class="img-fluid rounded shadow-lg" style="max-width: 400px;">
     </div>
 </div>
 @endsection
