90ba3467c317c6b701beaf7b9d3b3adf7a494d02
Santiago Idárraga Ceballos <san200526@gmail.com>
2025-03-24 02:07:20 -0500

    Created Product - Lang

---
+++ lang/en/product.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'title' => 'Our Products',
+    'title_site' => 'Our Products',
+    'filter' => 'Product Filter',
+    'sales_filter' => 'Sort by Sales:',
+    'price_filter' => 'Sort by Price:',
+    'asc' => 'Ascending',
+    'desc' => 'Descending',
+    'select' => 'Select',
+    'category' => 'Category',
+    'all_categories' => 'All Categories',
+    'brand_filter' => 'Brand',
+    'no_products' => 'No products found',
+    'price' => 'Price:',
+    'brand' => 'Brand:',
+    'sold_out' => 'Sold out',
+    'add_cart' => 'Add to cart',
+];
---
+++ lang/es/product.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'title' => 'Nuestros Products',
+    'title_site' => 'Nuestros Products',
+    'filter' => 'Filtros de Productos',
+    'sales_filter' => 'Ordernar por Ventas:',
+    'price_filter' => 'Ordenar por Precio:',
+    'asc' => 'Ascendente',
+    'desc' => 'Descendente',
+    'select' => 'Seleccionar',
+    'category' => 'Categoría',
+    'all_categories' => 'Todas las Categorías',
+    'brand_filter' => 'Marcas',
+    'price' => 'Precio',
+    'brand' => 'Marca',
+    'no_products' => 'No se encontraron productos',
+    'sold_out' => 'Agotado',
+    'add_cart' => 'Añadia al Carrito',
+];
