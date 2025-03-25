566ba5639b90b40ead7e2700a8eb48ae65f66ae9
Santiago Idárraga Ceballos <san200526@gmail.com>
2025-03-24 23:48:21 -0500

    Created Order Item model

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -2,9 +2,56 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class OrderItem extends Model
 {
-    //
+    /*
+     * Order ATTRIBUTES
+     * $this->attributes['id'] - int - contains the order primary key (id)
+     * $this->attributes['created_at'] - timestamp - contains the creation date of the order item
+     * $this->attributes['updated_at'] - timestamp - contains the last update date of the order item
+     * $this->user - User - contains the associated user
+     * $this->order - Order - contains the associated order
+    */
+
+    protected $fillable = ['order_id', 'product_id'];
+
+    public function getId(): int
+    {
+        return $this->attributes['id'];
+    }
+
+    public function order(): BelongsTo
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function getOrder(): Collection
+    {
+        return $this->order;
+    }
+
+    public function product(): HasOne
+    {
+        return $this->hasOne(Product::class);
+    }
+
+    public function getProduct(): Collection
+    {
+        return $this->product;
+    }
+
+    public function getCreatedAt(): string
+    {
+        return $this->attributes['created_at'];
+    }
+
+    public function getUpdatedAt(): string
+    {
+        return $this->attributes['updated_at'];
+    }
 }
