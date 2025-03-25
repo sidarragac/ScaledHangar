dfeba68a75d2ec26f082e48182e82682e3d01328
Santiago Idárraga Ceballos <101029953+sidarragac@users.noreply.github.com>
2025-03-24 22:02:33 -0500

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,6 +36,7 @@
           @csrf
         </form>
         @if(Auth::user()->is_admin)
+        <a class="nav-link active" href="{{route('admin.home')}}">{{__('navNames.admin')}}</a>
         @endif
         @endguest
       </ul>
