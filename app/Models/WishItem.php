90905ee310384918da8e59166445b1bc0a7fc0e9
Daviddgp04 <daviddgp04@hotmail.com>
2025-03-24 22:23:58 -0500

    fix: added return types WishItem.php

--- app/Models/WishItem.php
+++ app/Models/WishItem.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class WishItem extends Model
 {
@@ -43,12 +44,12 @@ class WishItem extends Model
         $this->attributes['product_id'] = $product_id;
     }
 
-    public function user()
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
 
-    public function product()
+    public function product(): BelongsTo
     {
         return $this->belongsTo(Product::class);
     }
