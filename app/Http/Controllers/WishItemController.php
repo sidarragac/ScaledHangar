317b7201f4255192cc4d9e6d4c6de8f35159b990
Daviddgp04 <daviddgp04@hotmail.com>
2025-03-24 19:51:04 -0500

    feature: admin panels

---
+++ app/Http/Controllers/WishItemController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+use App\Models\WishItem;
+
+class WishItemController extends Controller
+{
+    //
+}
--- routes/web.php
+++ routes/web.php
@@ -12,36 +12,45 @@ $homeControllerRoute = "App\Http\Controllers\HomeController";
 $adminDashboardControllerRoute = "App\Http\Controllers\Admin\DashboardController";
 
 Route::get('/', [$homeControllerRoute, 'index'])->name('home.index');
-
-Route::get('/admin', [$adminDashboardControllerRoute, 'index'])->middleware(['auth', 'admin'])->name('admin.dashboard');
-
 Auth::routes();
+//Admin Routes
+Route::prefix('admin')->name('admin.')->middleware(['auth', 'admin'])->group(function (){
+    $adminDashboardControllerRoute = "App\Http\Controllers\Admin\AdminHomeController";
+    Route::get('/', [$adminDashboardControllerRoute,'index'])->name('home');
+    // Category Routes
+    Route::prefix('categories')->name('category.')->group(function (){
+        $adminCategoryControllerRoute = 'App\Http\Controllers\Admin\AdminCategoryController@';
+        Route::get('/', $adminCategoryControllerRoute.'index')->name('index');
+        Route::get('/show/{id}', $adminCategoryControllerRoute.'show')->name('show');
+        Route::get('/create', $adminCategoryControllerRoute.'create')->name('create');
+        Route::post('/save', $adminCategoryControllerRoute.'save')->name('save');
+        Route::get('/edit/{id}', $adminCategoryControllerRoute.'edit')->name('edit');
+        Route::put('/update/{id}', $adminCategoryControllerRoute.'update')->name('update');
+        Route::delete('/delete/{id}', $adminCategoryControllerRoute.'delete')->name('delete');
 
-// Admin Category Routes
-
-Route::get('/admin/categories', $adminCategoryControllerRoute.'index')->name('admin.category.index');
-Route::get('/admin/categories/show/{id}', $adminCategoryControllerRoute.'show')->name('admin.category.show');
-Route::get('/admin/categories/create', $adminCategoryControllerRoute.'create')->name('admin.category.create');
-Route::post('/admin/categories/save', $adminCategoryControllerRoute.'save')->name('admin.category.save');
-Route::get('/admin/categories/edit/{id}', $adminCategoryControllerRoute.'edit')->name('admin.category.edit');
-Route::put('/admin/categories/update/{id}', $adminCategoryControllerRoute.'update')->name('admin.category.update');
-Route::delete('/admin/categories/delete/{id}', $adminCategoryControllerRoute.'delete')->name('admin.category.delete');
-
-// Admin Product Routes
-Route::get('/admin/products', $adminProductControllerRoute.'index')->name('admin.product.index');
-Route::get('/admin/products/show/{id}', $adminProductControllerRoute.'show')->name('admin.product.show');
-Route::get('/admin/products/create', $adminProductControllerRoute.'create')->name('admin.product.create');
-Route::post('/admin/products/save', $adminProductControllerRoute.'save')->name('admin.product.save');
-Route::get('/admin/products/edit/{id}', $adminProductControllerRoute.'edit')->name('admin.product.edit');
-Route::put('/admin/products/update/{id}', $adminProductControllerRoute.'update')->name('admin.product.update');
-Route::delete('/admin/products/delete/{id}', $adminProductControllerRoute.'delete')->name('admin.product.delete');
-
+    });
+    // Product Routes
+    Route::prefix('products')->name('product.')->group(function (){
+        $adminProductControllerRoute = 'App\Http\Controllers\Admin\AdminProductController@';
+        Route::get('/', $adminProductControllerRoute.'index')->name('index');
+        Route::get('/show/{id}', $adminProductControllerRoute.'show')->name('show');
+        Route::get('/create', $adminProductControllerRoute.'create')->name('create');
+        Route::post('/save', $adminProductControllerRoute.'save')->name('save');
+        Route::get('/edit/{id}', $adminProductControllerRoute.'edit')->name('edit');
+        Route::put('/update/{id}', $adminProductControllerRoute.'update')->name('update');
+        Route::delete('/delete/{id}', $adminProductControllerRoute.'delete')->name('delete');
+    });
+        
+});
 // Product Routes
 Route::get('/products', $productControllerRoute.'index')->name('product.index');
 
 // Cart Routes
-Route::get('/cart', $cartControllerRoute.'index')->name('cart.index');
-Route::get('/cart/add/{id}', $cartControllerRoute.'add')->name('cart.add');
-Route::get('/cart/remove/{id}', $cartControllerRoute.'remove')->name('cart.remove');
-Route::get('/cart/checkout', $cartControllerRoute.'checkout')->name('cart.checkout'); //Requires the user to be logged in.
-Route::get('/cart/clear', $cartControllerRoute.'clear')->name('cart.clear');
+Route::prefix('cart')->name('cart.')->middleware('auth')->group(function (){
+    $cartControllerRoute = 'App\Http\Controllers\CartController@';
+    Route::get('/', $cartControllerRoute.'index')->name('index');
+    Route::get('/add/{id}', $cartControllerRoute.'add')->name('add');
+    Route::get('/remove/{id}', $cartControllerRoute.'remove')->name('remove');
+    Route::get('/checkout', $cartControllerRoute.'checkout')->name('checkout'); //Requires the user to be logged in.
+    Route::get('/clear', $cartControllerRoute.'clear')->name('clear');
+});
