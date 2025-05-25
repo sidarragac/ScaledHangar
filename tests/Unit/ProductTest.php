6c00fa9896f148905a764cb7e2be7ffc6aa66f16
Santiago Idárraga Ceballos <san200526@gmail.com>
2025-05-25 06:00:46 -0500

    Created unitary test for the Product Model

---
+++ tests/Unit/ProductTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Product;
+use PHPUnit\Framework\TestCase;
+
+class ProductTest extends TestCase
+{
+    public function test_get_and_set_name(): void
+    {
+        $name = 'Test Product';
+        $product = new Product();
+        $product->setName($name);
+        $this->assertEquals($name, $product->getName());
+    }
+
+    public function test_get_and_set_price(): void
+    {
+        $price = 99.99;
+        $product = new Product();
+        $product->setPrice($price);
+        $this->assertEquals($price, $product->getPrice());
+    }
+}
