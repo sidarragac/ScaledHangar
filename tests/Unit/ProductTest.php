ce70ff0722f48adf765e40213d570b6887dbf425
sidarragac <101029953+sidarragac@users.noreply.github.com>
2025-05-25 11:01:03 +0000

    Linted Code with Pint

--- tests/Unit/ProductTest.php
+++ tests/Unit/ProductTest.php
@@ -10,7 +10,7 @@ class ProductTest extends TestCase
     public function test_get_and_set_name(): void
     {
         $name = 'Test Product';
-        $product = new Product();
+        $product = new Product;
         $product->setName($name);
         $this->assertEquals($name, $product->getName());
     }
@@ -18,7 +18,7 @@ class ProductTest extends TestCase
     public function test_get_and_set_price(): void
     {
         $price = 99.99;
-        $product = new Product();
+        $product = new Product;
         $product->setPrice($price);
         $this->assertEquals($price, $product->getPrice());
     }
