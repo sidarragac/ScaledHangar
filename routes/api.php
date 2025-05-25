172766e6bc4b4502e6e983addc7074ff5aa3c4dd
Santiago Idárraga Ceballos <san200526@gmail.com>
2025-05-25 01:42:05 -0500

    Created API routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+$productApiControllerRoute = 'App\Http\Controllers\Api\ProductApiController@';
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/products', $productApiControllerRoute.'index')->name('api.product.index');
+Route::get('/products/{id}', $productApiControllerRoute.'show')->name('api.product.show');
+Route::get('/products/stock', $productApiControllerRoute.'getProductsWithStock')->name('api.product.getProductsWithStock');
